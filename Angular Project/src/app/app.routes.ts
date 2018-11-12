d49841cf7947dada12296a4022c2f61826819fc6
Sunjoli Aggarwal <sunjoli.aggarwal@gmail.com>
2018-11-11 19:07:15 -0600

    Updated routes.ts

---
+++ Angular Project/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { AccountCreationComponent } from './account-creation/account-creation.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MatableComponent } from './matable/matable.component';
+
+// import { LoginComponent } from './login/login.component';
+// import { VehicleComponent } from './vehicle/vehicle.component';
+// import { ExpertComponent } from './expert/expert.component';
+// import { EquipmentComponent } from './equipment/equipment.component';
+// import { MatableComponent } from './matable/matable.component.ts';
+// import { AnstoryComponent } from './anstory/anstory.component.ts';
+
+
+
+export const APP_ROUTES: Routes = [
+ // { path: '/', component: Login},
+  { path: '/register', component: AccountCreationComponent },
+  { path: '/login', component: LoginComponent },
+  { path: '/home', component: HomepageComponent },
+  { path: '/manageAnchors', component: MatableComponent }
+ // { path: '/manageStories, component: AnstoryComponent }
+ // { path: '/manageVehicles', component: VehicleComponent },
+ // { path: '/manageEquipment', component: EquipmentComponent },
+ // { path: '/manageExperts', component: ExpertComponent },
+
+];
